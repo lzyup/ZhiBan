4976a77d7921b0b3fc800ba1cb4d142c2461b657
FelixXG <m15172547010@163.com>
2017-03-01 16:00:34 +0800

    no message

---
+++ app/src/main/java/com/felix/zhiban/presenter/DoubanBookDetailPresenter.java
@@ -0,0 +1,58 @@
+package com.felix.zhiban.presenter;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.felix.zhiban.api.Url;
+import com.felix.zhiban.base.BasePresenter;
+import com.felix.zhiban.bean.book.Books;
+import com.felix.zhiban.viewinterface.book.IGetBookDetailView;
+
+public class DoubanBookDetailPresenter extends BasePresenter {
+
+    private IGetBookDetailView iGetBookDetailView;
+
+
+    public DoubanBookDetailPresenter(Context mContext) {
+        super(mContext);
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void getBookById(IGetBookDetailView iGetBookDetailView1,String id){
+        iGetBookDetailView=iGetBookDetailView1;
+        GetBookByIdTask getBookByIdTask=new GetBookByIdTask();
+        getBookByIdTask.execute(id);
+    }
+
+
+    /**
+     * 根据id获取图书详情异步任务
+     */
+    private class GetBookByIdTask extends AsyncTask<String,Void,Books> {
+
+        @Override
+        protected Books doInBackground(String... strings) {
+            String id=strings[0];
+            Log.v("xgf121","进入获取图书详情");
+            Books mBooks =iNetWorkManager.getBookDetail(Url.GETBOOKDETAIL,id);
+            return mBooks;
+        }
+
+        @Override
+        protected void onPostExecute(Books books) {
+            super.onPostExecute(books);
+            displayBookDetail(iGetBookDetailView, books);
+        }
+    }
+
+
+    private void displayBookDetail(IGetBookDetailView iGetBookDetailView,Books books){
+        Log.v("xgf book","得到books");
+        iGetBookDetailView.getBookSuccess(books);
+    }
+}
