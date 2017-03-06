d533df070d133e3618899edc7e8836e2e927e114
FelixXG <m15172547010@163.com>
2017-03-06 17:05:16 +0800

    no message

--- app/src/main/java/com/felix/zhiban/viewinterface/book/IGetBookDetailView.java
+++ app/src/main/java/com/felix/zhiban/viewinterface/book/IGetBookDetailView.java
@@ -6,5 +6,11 @@ import com.felix.zhiban.bean.book.Books;
 public interface IGetBookDetailView {
     void getBookSuccess(Books books);
 
-    void getBookFail();
+   // void getBookFail();
+
+    void showContentView();
+
+    void showError();
+
+    void showLoading();
 }
