e20113cafc438d38d3574855ba7ef46b244ba6e8
FelixXG <m15172547010@163.com>
2017-03-07 15:44:37 +0800

    no message

--- app/src/main/java/com/felix/zhiban/presenter/book/DoubanBookDetailPresenter.java
+++ app/src/main/java/com/felix/zhiban/presenter/book/DoubanBookDetailPresenter.java
@@ -2,12 +2,11 @@ package com.felix.zhiban.presenter.book;
 
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
 
-import com.felix.zhiban.api.Url;
 import com.felix.zhiban.base.BasePresenter;
 import com.felix.zhiban.bean.book.Books;
+import com.felix.zhiban.model.book.DoubanBookDetailModel;
+import com.felix.zhiban.modelinterface.book.IDoubanBookDetailModel;
 import com.felix.zhiban.presenterinterface.book.IDoubanBookDetailPresenter;
 import com.felix.zhiban.viewinterface.book.IGetBookDetailView;
 
@@ -15,14 +14,12 @@ public class DoubanBookDetailPresenter extends BasePresenter implements IDoubanB
 
     private IGetBookDetailView iGetBookDetailView;
 
-
-    public DoubanBookDetailPresenter(Context mContext) {
-        super(mContext);
-    }
+    private IDoubanBookDetailModel iDoubanBookDetailModel;
 
     public DoubanBookDetailPresenter(Context mContext, IGetBookDetailView iGetBookDetailView) {
         super(mContext);
         this.iGetBookDetailView = iGetBookDetailView;
+        iDoubanBookDetailModel=new DoubanBookDetailModel(mContext,this);
     }
 
     /**
@@ -30,53 +27,28 @@ public class DoubanBookDetailPresenter extends BasePresenter implements IDoubanB
      * @param id
      */
     @Override
-    public void getBookById(IGetBookDetailView iGetBookDetailView1, String id) {
-            iGetBookDetailView=iGetBookDetailView1;
-            GetBookByIdTask getBookByIdTask=new GetBookByIdTask();
-            getBookByIdTask.execute(id);
+    public void getBookById( String id) {
+        iDoubanBookDetailModel.initBookById(id);
+
     }
 
+    @Override
+    public void getBookByIdSuccess(Books books) {
 
-
-
-
-    /**
-     * 根据id获取图书详情异步任务
-     */
-    private class GetBookByIdTask extends AsyncTask<String,Void,Books> {
-
-        @Override
-        protected Books doInBackground(String... strings) {
-            String id=strings[0];
-            Log.v("xgf121","进入获取图书详情");
-            Books mBooks =iNetWorkManager.getBookDetail(Url.GETBOOKDETAIL,id);
-            return mBooks;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            iGetBookDetailView.showLoading();
-        }
-
-        @Override
-        protected void onPostExecute(Books books) {
-            super.onPostExecute(books);
-            if(books==null){
-
-                iGetBookDetailView.showError();
-            }else{
-                iGetBookDetailView.showContentView();
-                displayBookDetail(iGetBookDetailView, books);
-            }
-
-        }
-    }
-
-
-    private void displayBookDetail(IGetBookDetailView iGetBookDetailView,Books books){
-        Log.v("xgf book","得到books");
+        iGetBookDetailView.showContentView();
         iGetBookDetailView.getBookSuccess(books);
     }
+
+    @Override
+    public void getBookByIdReady() {
+        iGetBookDetailView.showLoading();
+    }
+
+    @Override
+    public void getBookByIdFail() {
+
+        iGetBookDetailView.showError();
+    }
+
 
 }
